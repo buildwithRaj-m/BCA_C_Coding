4d243a98618a9d220a86e113865ca24b76ee73f8
buildwithRaj-m <rajmaurya.tech@gmail.com>
2026-02-10 13:49:47 +0530

    Revising basic addition problems

---
+++ new.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+    int a;
+float b;
+    printf("ENTER a:\n");
+    scanf("%d", & a);
+     printf("ENTER b:\n");
+     scanf("%f",& b);
+      float c=a+b;
+    printf("Sum of a and b is:%f\n",c);
+    return 0;
+}
