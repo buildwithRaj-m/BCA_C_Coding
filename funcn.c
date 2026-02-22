83d8300d7cc410c292cb3b9017fb599ed7616540
buildwithRaj-m <rajmaurya.tech@gmail.com>
2026-02-22 13:15:59 +0530

    Functions basic revision

---
+++ funcn.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+//Declaration
+void printHello();
+
+int main() {
+
+    printHello();//Function Call
+    return 0;
+}
+//Definition
+void printHello() {
+    printf("Hello!!!");
+}
