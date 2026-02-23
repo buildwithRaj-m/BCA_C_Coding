ba1fc185ed67fb1bedda63ba87cb74a89eec7806
buildwithRaj-m <rajmaurya.tech@gmail.com>
2026-02-23 14:42:44 +0530

    Basic question practice on recursion

---
+++ recur.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void printHW(int count);
+
+int main() {
+    int n;
+    printf("Enter number:");
+    scanf("%d", & n);
+
+    printHW(n);
+
+    return 0;
+}
+
+void printHW(int count){
+    if(count==0){
+        return;
+    }
+    printf("Hello World!!\n");
+    printHW(count-1);
+}
