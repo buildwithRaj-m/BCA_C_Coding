f4e19105d4f04c7fec07bb946b7f0f2aed50786c
buildwithRaj-m <rajmaurya.tech@gmail.com>
2026-02-25 23:02:44 +0530

    call by value practice

---
+++ ref.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void printAddress(int n);
+
+int main() {
+  int n = 4;
+
+  printf("%p\n", &n);
+  printAddress(n);
+  return 0;
+}
+
+void printAddress(int n){  //call by value
+  printf("%p\n", &n);
+}
