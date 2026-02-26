7a77ac8fb8814dd21a516b3361eb9aa7f0aad392
buildwithRaj-m <rajmaurya.tech@gmail.com>
2026-02-26 12:16:05 +0530

    Arrays practice

---
+++ arrays1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main() {
+    float price[3] = {499.0 ,599.0 ,202.0};
+    
+
+    printf("Total price without gst :%.2f\n", price[0]+price[1]+price[2]);
+
+    float f_price1 = price[0]+(price[0]*0.18);
+    float f_price2 = price[1]+(price[1]*0.18);
+    float f_price3 = price[2]+(price[2]*0.18);
+    float final = f_price1 + f_price2 + f_price3;
+
+    printf("Final amount with gst : %.2f\n",final);
+    printf("Thank you :)\n");
+    return 0;
+
+}
